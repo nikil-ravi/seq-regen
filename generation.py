fixed masking, unmasking

generation.py
@@ -1,14 +1,17 @@
 import transformers
-from transformers import pipeline, set_seed, GPT2Tokenizer, GPT2Model, BertModel
+from transformers import pipeline, set_seed, GPT2Tokenizer, GPT2Model, BertModel, BertTokenizer
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 import numpy as np
 import math
 
 # returns masked token list, along with corresponding indices that need to be regenerated
-def mask_out_tokens(tokens, percentage=0.15):
+def mask_out_tokens(text, percentage=0.15):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    tokens = tokenizer.tokenize(text)
     mask_indices = np.random.choice(range(len(tokens)), math.floor(percentage * len(tokens)), replace=False)
     tokens = [tokens[i] if i not in mask_indices else "[MASK]" for i in range(len(tokens))]
-    return tokens, mask_indices
+    decoded_text = " ".join(tokens)
+    return decoded_text, mask_indices
 
 def unmask(text, model="bert-base-uncased"):
     # TODO: might error out with non-MLM models- handle this
@@ -27,7 +30,7 @@ def get_text_features(text, config):
         tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
         model = GPT2Model.from_pretrained('gpt2')
     elif model_config == "bert-base-uncased":
-        tokenizer = GPT2Tokenizer.from_pretrained('bert-base-uncased')
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         model = BertModel.from_pretrained("bert-base-uncased")
     elif model_config == "bert-large-uncased-whole-word-masking-finetuned-squad":
         tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")